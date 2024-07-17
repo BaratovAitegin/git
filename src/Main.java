1e0af7696b30fdb7c26fc1bd7c1750176f68cee4
Aitegin Baratov <redmifly@gmail.com>
2024-07-17 19:41:35 +0600

    Зафиксировано второе изменение

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Мы сегодня изучаем версионностть - Git");
+        System.out.println("Новая строка для демонстрации");
     }
 }
