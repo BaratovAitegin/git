e08d6cca4653e6592de4df4135ca21d1716e909f
aiteginbaratov <redmifly@gmail.com>
2024-07-17 20:06:47 +0600

    Добавил новую строку чтобы понять как работает GIT

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Мы сегодня изучаем версионностть - Git");
         System.out.println("Новая строка для демонстрации");
+        System.out.println("Добавление новой строки");
     }
 }
